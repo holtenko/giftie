19a0103a9238a7010280f6b42d5f157ca221f945
holten <holten.ko@gmail.com>
2024-02-08 13:28:23 +0800

    update

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -15,7 +15,7 @@ export const homeConfig = {
   // 留言，数组的每一项代表一行
   messages: [
     { key: 'a', wording: 'Only for Rita', },
-    { key: 's', wording: '2023.02.14', },
+    { key: 's', wording: '2024.02.14', },
   ],
   // 最终解释权归属人
   owner: 'Holten',
@@ -31,9 +31,9 @@ export const homeConfig = {
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
   { key: 'q', name: '全身按摩(30min)', alias: '按摩(30min)', image: './images/spa.png',  description: '「奢华享受, 感受轻松」' },
-  { key: 'e', name: 'TASAKI BalanceCharm项链', alias: 'TASAKI', image: './images/tasaki.png',  description: '「定格优雅, 风格隽永」' },
   { key: 'r', name: '全定制旅行', alias: '全定制旅行', image: './images/travel.png',  description: '「说走就走, 私享定制」' },
   { key: 'i', name: 'LAMER精华乳霜30ml', alias: 'LAMER乳霜', image: './images/lamer.png',  description: '「柔韧修护, 纵享年轻」' },
+  { key: 'e', name: '专车券x3', alias: '专车券x3', image: './images/car.png',  description: '「风雨同舟, 伴你一路」' },
   { key: 'w', name: '黄金(10g)', alias: '黄金(10g)', image: './images/gold.png',  description: '「金诚可贵, 情比金坚」' },
   { key: 't', name: '洗碗券x12', alias: '洗碗券x12', image: './images/wash.png',  description: '「每月一次, 轻松自在」' },
   { key: 'y', name: '1314红包', alias: '1314', image: './images/money.png',  description: '「从今往后，我都会在你旁边」' },
